cc5b0f7456c60d3751ed3d02f7f92b4104fbe549
Ashish Chopra <ashish1194.cse16@chitkara.edu.in>
2020-04-06 18:19:23 +0530

    retrieve table data using invoke SQLCmd

---
+++ Connecting sql with PS/Invoke SQLCmd redhat.ps1
@@ -0,0 +1,6 @@
+﻿$tables= Invoke-Sqlcmd -query 'Select * from dbo.baskets' -ServerInstance CYG362 -Database sql_hr  
+write-host ($tables | Format-Table | Out-String) 
+$tables | Out-GridView 
+
+$databases = invoke-sqlcmd -server $CYG362 'select name from master.sys.databases where database_id > 4 order by name' 
+write-host ($databases | Format-Table | Out-String) 
